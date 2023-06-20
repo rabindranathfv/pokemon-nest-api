280ff6f1607cd4a8c774099f8f12959041d51873
Rabindranath Ferreira <ferreiralox@gmail.com>
2023-06-20 11:10:49 +0200

    make insertMany using seed an external API

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from 'src/pokemon/entities/pokemon.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name,
+        schema: PokemonSchema,
+      },
+    ]),
+  ],
   controllers: [SeedController],
-  providers: [SeedService]
+  providers: [SeedService],
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interface/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon, PokemonDocument } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<PokemonDocument>,
+  ) {}
+
   async pokemonRunSeeder() {
+    const checkData = await this.pokemonModel.find();
+
+    if (checkData.length > 0) {
+      return `your pokemon seed it's already DONE`;
+    }
+
     const { data } = await this.axios.get<PokeResponse>(
       `https://pokeapi.co/api/v2/pokemon?limit=650`,
     );
@@ -20,10 +34,9 @@ export class SeedService {
         number: pokemonNum,
       };
     });
-    console.log(
-      '🚀 ~ file: seed.service.ts:20 ~ SeedService ~ pokemonMapped ~ pokemonMapped:',
-      pokemonMapped,
-    );
-    return data.results;
+
+    const insertedPokemons = await this.pokemonModel.insertMany(pokemonMapped);
+
+    return insertedPokemons;
   }
 }
